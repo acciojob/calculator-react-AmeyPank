f7b03182e8a1dcb0e86005105b786f8f07f7decb
AmeyPank <pankantiameya@gmail.com>
2023-05-14 14:48:48 +0530

    Calcualtor

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,50 +1,53 @@
 import React, { useState } from 'react';
-import "../styles/App.css"
-function Calculator() {
-  const [result, setResult] = useState('');
+import '../styles/App.css';
 
-  const handleClick = (event) => {
-    setResult(result.concat(event.target.name));
+function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
   }
 
   const clear = () => {
-    setResult('');
+    setResult("");
   }
 
   const calculate = () => {
     try {
       setResult(eval(result).toString());
     } catch (error) {
-      setResult('Error');
+      setResult("Error");
     }
   }
 
   return (
-    <div className="calculator">
-      <input  id='input' type="text" value={result} />
-      <div className="keypad">
-      <button name="C" onClick={clear}>C</button>
-        <button name="/" onClick={handleClick}>/</button>
-        <button name="*" onClick={handleClick}>*</button>
-        <button name="+" onClick={handleClick}>+</button>
-        <button name="7" onClick={handleClick}>7</button>
-        <button name="8" onClick={handleClick}>8</button>
-        <button name="9" onClick={handleClick}>9</button>
-        <button name="-" onClick={handleClick}>-</button>
-        <button name="4" onClick={handleClick}>4</button>
-        <button name="5" onClick={handleClick}>5</button>
-        <button name="6" onClick={handleClick}>6</button>
-        <button name="=" onClick={calculate}>=</button>
-        <button name="1" onClick={handleClick}>1</button>
-        <button name="2" onClick={handleClick}>2</button>
-        <button name="3" onClick={handleClick}>3</button>
-        <button name="0" onClick={handleClick}>0</button>
-        
-        
-
+    <div className="App">
+      <div id='calci' className='Calculator'>
+        <form>
+            <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+            <button onClick={clear} id="clear">C</button>
+            <button onClick={handleClick} id='btn-div' name="/" className="operator">/</button>
+            <button onClick={handleClick} name="*" className="operator">*</button>
+            <button onClick={handleClick} id="btn--" name="-" className="operator">-</button>
+            <button onClick={handleClick} id='btn-7' name="7">7</button>
+            <button onClick={handleClick} id='btn-8' name="8">8</button>
+            <button onClick={handleClick} id='btn-9' name="9">9</button>
+            <button onClick={handleClick} name="+" className="operator" id='plus' aria-rowspan={2}>+</button>
+            <button onClick={handleClick} id='btn-4' name="4">4</button>
+            <button onClick={handleClick} id='btn-5' name="5">5</button>
+            <button onClick={handleClick} id='btn-6' name="6">6</button>
+            <button onClick={handleClick} id='btn-1' name="1">1</button>
+            <button onClick={handleClick} id='btn-2' name="2">2</button>
+            <button onClick={handleClick} id='btn-3' name="3">3</button>
+            <button onClick={calculate} id="equal">=</button>
+            <button onClick={handleClick} id='btn-0' name="0">0</button>
+            <button onClick={handleClick} name=".">.</button>
+        </div>
       </div>
     </div>
   );
 }
 
-export default Calculator;
+export default App;
