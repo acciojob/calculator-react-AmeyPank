effdf3ddda4f0207eb32097ed28fa037ef6bd888
AmeyPank <pankantiameya@gmail.com>
2023-05-16 01:02:43 +0530

    Test cases

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import App from "./components/App";
 
 ReactDOM.render(<App/>, document.getElementById("root"))
 
+// "scripts": {
+//     "test": "jest --no-color 2>output.txt",
+//     "start": "webpack-dev-server --mode development --watch --open --hot",
+//     "build": "webpack --mode production"
+//   },
