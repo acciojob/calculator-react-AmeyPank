008852495d1beae87327855234c0332d2c83e254
AmeyPank <pankantiameya@gmail.com>
2023-06-15 01:57:44 +0530

    Push

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,87 @@
-import React, { useState } from 'react';
-import '../styles/App.css';
-import * as math from 'mathjs';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [result, setResult] = useState('');
+const App = () => {
+  const [display, setDisplay] = useState("");
+  const [result, setResult] = useState("");
 
-  const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
-  };
-
-  const clear = () => {
-    setResult('');
-  };
-
-  const calculate = () => {
-    try {
-      const expression = math.evaluate(result);
-      setResult(expression.toString());
-    } catch (error) {
-      setResult('Error');
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        const evalResult = eval(display);
+        setResult(evalResult);
+        setDisplay(evalResult);
+      } catch (error) {
+        setResult("Error");
+        setDisplay("");
+      }
+    } else if (value === "C") {
+      setDisplay("");
+      setResult("");
+    } else {
+      setDisplay((prevDisplay) => prevDisplay + value);
     }
   };
 
   return (
-    <div className="App">
-      <div id='calci' className='Calculator'>
-        <form>
-          <input type="text" value={result} />
-        </form>
-        <div className="keypad">
-          <button onClick={clear} id="clear">C</button>
-          <button onClick={handleClick} id='btn-div' name="/" className="operator">/</button>
-          <button onClick={handleClick} name="*" className="operator">*</button>
-          <button onClick={handleClick} id="btn--" name="-" className="operator">-</button>
-          <button onClick={handleClick} id='btn-7' name="7">7</button>
-          <button onClick={handleClick} id='btn-8' name="8">8</button>
-          <button onClick={handleClick} id='btn-9' name="9">9</button>
-          <button onClick={handleClick} name="+" className="operator" id='plus' aria-rowspan={2}>+</button>
-          <button onClick={handleClick} id='btn-4' name="4">4</button>
-          <button onClick={handleClick} id='btn-5' name="5">5</button>
-          <button onClick={handleClick} id='btn-6' name="6">6</button>
-          <button onClick={handleClick} id='btn-1' name="1">1</button>
-          <button onClick={handleClick} id='btn-2' name="2">2</button>
-          <button onClick={handleClick} id='btn-3' name="3">3</button>
-          <button onClick={calculate} id="equal">=</button>
-          <button onClick={handleClick} id='btn-0' name="0">0</button>
-          <button onClick={handleClick} name=".">.</button>
-        </div>
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={display} readOnly />
       </div>
+      {/* <div className="buttons">
+        <button onClick={() => handleClick("7")}>7</button>
+        <button onClick={() => handleClick("8")}>8</button>
+        <button onClick={() => handleClick("9")}>9</button>
+        <button onClick={() => handleClick("/")}>/</button>
+        <button onClick={() => handleClick("4")}>4</button>
+        <button onClick={() => handleClick("5")}>5</button>
+        <button onClick={() => handleClick("6")}>6</button>
+        <button onClick={() => handleClick("*")}>*</button>
+        <button onClick={() => handleClick("1")}>1</button>
+        <button onClick={() => handleClick("2")}>2</button>
+        <button onClick={() => handleClick("3")}>3</button>
+        <button onClick={() => handleClick("-")}>-</button>
+        <button onClick={() => handleClick("0")}>0</button>
+        <button onClick={() => handleClick(".")}>.</button>
+        <button onClick={() => handleClick("=")}>=</button>
+        <button onClick={() => handleClick("+")}>+</button>
+        <button onClick={() => handleClick("C")}>C</button>
+      </div> */}
+      {/* <div className="result">
+        <p>{result}</p>
+      </div> */}
+
+      <table>
+        <tr>
+          <button onClick={() => handleClick("C")}>C</button>
+          <button onClick={() => handleClick("/")}>/</button>
+          <button onClick={() => handleClick("*")}>*</button>
+          <button onClick={() => handleClick("-")}>-</button>
+        </tr>
+        <tr>
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("+")}>+</button>
+        </tr>
+        <tr>
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+        </tr>
+        <tr>
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+        </tr>
+        <tr>
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick(".")}>.</button>
+          <button onClick={() => handleClick("=")}>=</button>
+        </tr>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
